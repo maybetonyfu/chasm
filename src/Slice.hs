a976de26fa13311340009366896360f479a73246
Tony Fu <tonyfu@fastmail.com>
2022-10-07 22:41:31 +1100

    Update style

--- src/Slice.hs
+++ src/Slice.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
@@ -20,7 +19,6 @@ data Slices = Slices
   }
 
 instance HasLogFunc Slices where
-  logFuncL :: Lens' Slices LogFunc
   logFuncL = lens logFun (\x y -> x {logFun = y})
 
 sc :: SrcSpanInfo -> Range
@@ -44,48 +42,48 @@ instance HasSlice Module where
   makeSlices _ _ = error "Not a module"
 
 instance HasSlice Decl where
-  makeSlices parent (PatBind srcspan pat rhs maybeWheres) = do
-    addSlices parent (getNames pat)
+  makeSlices p (PatBind srcspan pat rhs maybeWheres) = do
+    addSlices p (getNames pat)
     makeSlices (sc srcspan) rhs
     mapM_ (makeSlices (sc srcspan)) maybeWheres
-  makeSlices parent (FunBind srcspan matches) = do
-    mapM_ (makeSlices parent) matches
-  makeSlices _ p = error ("Node type not support: " ++ show p)
+  makeSlices p (FunBind srcspan matches) = do
+    mapM_ (makeSlices p) matches
+  makeSlices p node = error ("Node type not support: " ++ show node)
 
 instance HasSlice Match where
-  makeSlices parent (Match srcspan name pats rhs maybeWheres) = do
-    addSlices parent (getNames name)
+  makeSlices p (Match srcspan name pats rhs maybeWheres) = do
+    addSlices p (getNames name)
     addSlices (sc srcspan) (getNames pats)
     makeSlices (sc srcspan) rhs
     mapM_ (makeSlices (sc srcspan)) maybeWheres
-  makeSlices parent (InfixMatch srcInfo pat name pats rhs maybeWheres) =
-    makeSlices parent (Match srcInfo name (pat : pats) rhs maybeWheres)
+  makeSlices p (InfixMatch srcInfo pat name pats rhs maybeWheres) =
+    makeSlices p (Match srcInfo name (pat : pats) rhs maybeWheres)
 
 instance HasSlice Rhs where
-  makeSlices _ (UnGuardedRhs srcspan exp) = makeSlices (sc srcspan) exp
-  makeSlices _ p = error ("Node type not support: " ++ show p)
+  makeSlices p (UnGuardedRhs srcspan exp) = makeSlices (sc srcspan) exp
+  makeSlices p node = error ("Node type not support: " ++ show node)
 
 instance HasSlice Exp where
-  makeSlices _ (InfixApp srcspan e1 _ e2) = mapM_ (makeSlices (sc srcspan)) [e1, e2]
-  makeSlices _ (App srcspan e1 e2) = mapM_ (makeSlices (sc srcspan)) [e1, e2]
-  makeSlices _ (NegApp srcspan e) = makeSlices (sc srcspan) e
-  makeSlices _ (Lambda srcspan pats e) = addSlices (sc srcspan) (getNames pats) >> makeSlices (sc srcspan) e
-  makeSlices _ (Let srcspan bind e) = makeSlices (sc srcspan) bind >> makeSlices (sc srcspan) e
-  makeSlices _ (If srcspan e1 e2 e3) = mapM_ (makeSlices (sc srcspan)) [e1, e2, e3]
-  makeSlices _ (Case srcspan exp alts) = makeSlices (sc srcspan) exp >> mapM_ (makeSlices (sc srcspan)) alts
-  makeSlices _ (Tuple srcspan _ exps) = mapM_ (makeSlices (sc srcspan)) exps
-  makeSlices _ (List srcspan exps) = mapM_ (makeSlices (sc srcspan)) exps
-  makeSlices _ (Paren srcspan e) = makeSlices (sc srcspan) e
-  makeSlices _ (LeftSection srcspan e _) = makeSlices (sc srcspan) e
-  makeSlices _ (RightSection srcspan _ e) = makeSlices (sc srcspan) e
-  makeSlices _ node = error $ "Unsupported node type: " ++ show node
+  makeSlices p (InfixApp srcspan e1 _ e2) = mapM_ (makeSlices (sc srcspan)) [e1, e2]
+  makeSlices p (App srcspan e1 e2) = mapM_ (makeSlices (sc srcspan)) [e1, e2]
+  makeSlices p (NegApp srcspan e) = makeSlices (sc srcspan) e
+  makeSlices p (Lambda srcspan pats e) = addSlices (sc srcspan) (getNames pats) >> makeSlices (sc srcspan) e
+  makeSlices p (Let srcspan bind e) = makeSlices (sc srcspan) bind >> makeSlices (sc srcspan) e
+  makeSlices p (If srcspan e1 e2 e3) = mapM_ (makeSlices (sc srcspan)) [e1, e2, e3]
+  makeSlices p (Case srcspan exp alts) = makeSlices (sc srcspan) exp >> mapM_ (makeSlices (sc srcspan)) alts
+  makeSlices p (Tuple srcspan _ exps) = mapM_ (makeSlices (sc srcspan)) exps
+  makeSlices p (List srcspan exps) = mapM_ (makeSlices (sc srcspan)) exps
+  makeSlices p (Paren srcspan e) = makeSlices (sc srcspan) e
+  makeSlices p (LeftSection srcspan e _) = makeSlices (sc srcspan) e
+  makeSlices p (RightSection srcspan _ e) = makeSlices (sc srcspan) e
+  makeSlices p node = error $ "Unsupported node type: " ++ show node
 
 instance HasSlice Binds where
-  makeSlices parent (BDecls _ decls) = mapM_ (makeSlices parent) decls
-  makeSlices _ p = error ("Node type not support: " ++ show p)
+  makeSlices p (BDecls _ decls) = mapM_ (makeSlices p) decls
+  makeSlices p node = error ("Node type not support: " ++ show node)
 
 instance HasSlice Alt where
-  makeSlices parent (Alt srcspan pat rhs maybeWheres) = do
+  makeSlices p (Alt srcspan pat rhs maybeWheres) = do
     addSlices (sc srcspan) (getNames pat)
     makeSlices (sc srcspan) rhs
     mapM_ (makeSlices (sc srcspan)) maybeWheres
